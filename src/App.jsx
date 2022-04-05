fb85fb5775fe4669db8cf073d60f09bcb1aad755
Melvin A <melvinalmonte@gmail.com>
2022-04-04 20:30:37 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+import "./App.css";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+export const data = {
+  labels: [
+    "Pillar 1",
+    "Pillar 2",
+    "Pillar 3",
+    "Pillar 4",
+    "Pillar 5",
+    "Pillar 6",
+  ],
+  datasets: [
+    {
+      label: "Your score",
+      data: [80, 90, 60, 54, 20, 70],
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgba(255, 99, 132, 1)",
+      borderWidth: 1,
+    },
+    {
+      label: "Avg score",
+      data: [90, 80, 70, 70, 96, 88],
+      fill: true,
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+      borderColor: "rgb(54, 162, 235)",
+      pointBackgroundColor: "rgb(54, 162, 235)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgb(54, 162, 235)",
+    },
+  ],
+};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="radar-container">
+          <Radar data={data} />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
